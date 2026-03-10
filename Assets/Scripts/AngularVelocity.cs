b787fd27cd5e9d691702d021e843cb08c5694e1d
Kong5109 <kong8071.kk@gmail.com>
2026-03-10 15:52:25 +0700

    Magnus Effect Scene Setup

--- Assets/Scripts/AngularVelocity.cs
+++ Assets/Scripts/AngularVelocity.cs
@@ -19,7 +19,7 @@ public class AngularVelocity : MonoBehaviour
         }
         else
         {
-            rb.angularVelocity = Vector3.zero;
+           rb.angularVelocity = Vector3.zero;
         }
     }
 }
